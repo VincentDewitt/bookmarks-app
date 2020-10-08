c9ed449ccc0e2414034b3954d9e3db71dfbdb124
Vincent Cataldo <vincentcataldo@me.com>
2020-10-08 14:57:29 -0700

    initial commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_TOKEN:"3b030d9b-733f-431a-bef2-18edb28f6727"
+}
